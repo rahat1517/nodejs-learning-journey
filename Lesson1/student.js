eac162a15d4de8b2f57b9e10b74b8a13e895ed06
G. M. Rashidul Islam Rahat <gmrashidulislam003@gmail.com>
2025-11-05 11:16:25 +0600

    Initial commit: Node.js learning journey started

---
+++ Lesson1/student.js
@@ -0,0 +1,15 @@
+const getName = () =>{
+    return "Rahat";
+}
+
+const getage = () =>{
+    return "25";
+}
+
+// exports.getName = getName;
+// exports.getage = getage;
+
+module.exports ={
+    getName,
+    getage
+}
